9501abffa6a9856eceff18401f43d04efbf8d90d
Michael John Dollente <michaeljohn.dollente@tup.edu.ph>
2021-01-11 02:33:01 +0800

    Laravel Passport

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Validator;
+use Hash;
+use App\Models\User;
+use Auth;
+
+class ApiController extends Controller
+{
+    public function register (Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return response()->json(['error' => $validator->errors()]);
+        }
+
+        $newuser = $request->all();
+        $newuser['password'] = Hash::make($newuser['password']);
+        $user = User::create($newuser);
+        $success['token'] = $user->createToken('AppName')->accessToken;
+
+        return response()->json(['success'=>$success], 200);
+    }
+
+    public function login(Request $request)
+    {
+        $crdtls =[
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if(auth()->attempt($crdtls)){
+            $user = Auth::user();
+
+            $success['token'] = $user->createToken('AppName')->accessToken;
+            return response()->json(['success' => $success], 200);
+        }
+
+        else{
+            return response()->json(['error'=>'Unauthorized'], 401);
+        }
+    }
+
+    public function user_info()
+    {
+        $user = Auth::user();
+
+        return response()->json(['success' => $user], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ApiController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('login', 'ApiController@login');
+Route::post('register', 'ApiController@register');
+Route::middleware('auth:api')->group(function(){
+    Route::post('details', 'ApiController@user_info');
 });
