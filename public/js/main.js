30ee5c846489ae8d35e4c3e73675f010b8fd839f
Michael John Dollente <michaeljohn.dollente@tup.edu.ph>
2021-01-13 19:32:46 +0800

    AJAX

--- public/js/main.js
+++ public/js/main.js
@@ -47,7 +47,6 @@ $('.link').on('click', (function (e) {
                         `)
                     });
                     $('#con').append(modals.movie);
-
                     break;
 
                     case 'actor':
@@ -85,102 +84,6 @@ $('.link').on('click', (function (e) {
                         `)
                     });
                     break;
-
-                    case 'genre':
-                    template = ` 
-                    <title>Genres</title>
-                    <h1>Genres</h1>
-                    <button class="btn btn-primary" type="submit">Add Genre</button><br><br>
-                    <table class="table table-hover mx-auto">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Name</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody id="genreData">
-
-                        </tbody>
-                    </table>
-                    `;
-                    $('#con').html(template);
-                    response.forEach(element => {
-                        $('#genreData').append(`
-                        <tr>
-                            <td>${element.id}</td>
-                            <td>${element.name}</td>
-                        </tr>
-                        `)
-                    });
-                    break;
-
-                    case 'producer':
-                    template = ` 
-                    <h1>Producers</h1>
-                    <button class="btn btn-primary" type="submit">Add Producer</button><br><br>
-                    <table class="table table-hover mx-auto">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Name</th>
-                                <th>E-Mail</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody id="producerData">
-
-                        </tbody>
-                    </table>
-                    `;
-                    $('#con').html(template);
-                    response.forEach(element => {
-                        $('#producerData').append(`
-                        <tr>
-                            <td>${element.id}</td>
-                            <td>${element.name}</td>
-                            <td>${element.email}</td>
-                        </tr>
-                        `)
-                    });
-                    break;
-
-                    case 'role':
-                    template = ` 
-                    <h1>Movies</h1>
-                    <button class="btn btn-primary" type="submit">Add Movie</button><br><br>
-                    <table class="table table-hover mx-auto">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Image</th>
-                                <th>Title</th>
-                                <th>Description</th>
-                                <th>Release</th>
-                                <th>Genre</th>
-                                <th>Producer</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody id="movieData">
-                        </tbody>
-                    </table>
-                    `;
-                    $('#con').html(template);
-                    response.forEach(element => {
-                        $('#movieData').append(`
-                        <tr>
-                            <td>${element.id}</td>
-                            <td>${element.imgpath}</td>
-                            <td>${element.title}</td>
-                            <td>${element.description}</td>
-                            <td>${element.release}</td>
-                            <td>${element.genre_id}</td>
-                            <td>${element.producer_id}</td>
-                        </tr>
-                        `)
-                    });
-                    break;
                 default:
                     break;
             }
@@ -189,11 +92,10 @@ $('.link').on('click', (function (e) {
 }));
 const modals = {
     movie:`
-
     <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
       <div class="modal-dialog modal-lg">
         <div class="modal-content">
-          <p>kinginamo</p>
+            <h1>Create New Movie</h1>
         </div>
       </div>
     </div>`
